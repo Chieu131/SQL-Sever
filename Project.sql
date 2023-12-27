7b40e81e5c28363bcb0056c7cf4d76ebe8f867de
Chieu131 <142771810+Chieu131@users.noreply.github.com>
2023-12-27 11:52:34 +0700

    Update Project.sql

--- Project.sql
+++ Project.sql
@@ -45,7 +45,11 @@ CREATE TABLE GroupDetail (
 GO
 DROP  database LAB12
 -- Ví dụ thêm dữ liệu cho bảng Employee
-INSERT INTO Employee (EmployeeID, Name, Tel, Email) VALUES (1, 'Nguyen Van A', '0123456789', 'nva@example.com');
+INSERT INTO Employee (EmployeeID, Name, Tel, Email) VALUES (1, 'Nguyen Van A', '01234567891', '1nva@example.com');
+INSERT INTO Employee (EmployeeID, Name, Tel, Email) VALUES (2, 'Nguyen Van B', '01234567892', '2nva@example.com');
+INSERT INTO Employee (EmployeeID, Name, Tel, Email) VALUES (3, 'Nguyen Van C', '01234567893', '3nva@example.com');
+INSERT INTO Employee (EmployeeID, Name, Tel, Email) VALUES (4, 'Nguyen Van D', '01234567894', '4nva@example.com');
+
 -- Lặp lại tương tự cho các bảng khác.
 
 Go
@@ -55,23 +59,23 @@ SELECT * FROM Employee;
 -- b. Liệt kê danh sách nhân viên đang làm dự án “Chính phủ điện tử”
 SELECT e.* FROM Employee e
 JOIN GroupDetail gd ON e.EmployeeID = gd.EmployeeID
-JOIN Group g ON gd.GroupID = g.GroupID
+JOIN Groups g ON gd.GroupID = g.GroupID
 JOIN Project p ON g.ProjectID = p.ProjectID
 WHERE p.ProjectName = 'Chính phủ điện tử';
 
 -- c. Thống kê số lượng nhân viên đang làm việc tại mỗi nhóm
 SELECT g.GroupID, COUNT(e.EmployeeID) AS NumberOfEmployees
-FROM Group g
+FROM Groups g
 JOIN GroupDetail gd ON g.GroupID = gd.GroupID
 JOIN Employee e ON gd.EmployeeID = e.EmployeeID
 GROUP BY g.GroupID;
 
 -- d. Liệt kê thông tin cá nhân của các trưởng nhóm
 SELECT e.* FROM Employee e
-JOIN Group g ON e.EmployeeID = g.LeaderID;
+JOIN Groups g ON e.EmployeeID = g.LeaderID;
 
 -- e. Liệt kê thông tin về nhóm và nhân viên đang làm các dự án có ngày bắt đầu làm trước ngày 12/10/2010
-SELECT g.*, e.* FROM Group g
+SELECT g.*, e.* FROM Groups g
 JOIN GroupDetail gd ON g.GroupID = gd.GroupID
 JOIN Employee e ON gd.EmployeeID = e.EmployeeID
 JOIN Project p ON g.ProjectID = p.ProjectID
@@ -85,7 +89,7 @@ WHERE gd.Status = 'sắp làm';
 -- g. Liệt kê tất cả thông tin về nhân viên, nhóm làm việc, dự án của những dự án đã hoàn thành
 SELECT e.*, g.*, p.* FROM Employee e
 JOIN GroupDetail gd ON e.EmployeeID = gd.EmployeeID
-JOIN Group g ON gd.GroupID = g.GroupID
+JOIN Groups g ON gd.GroupID = g.GroupID
 JOIN Project p ON g.ProjectID = p.ProjectID
 WHERE p.EndDate IS NOT NULL;
 
